7c66f24ac3e4172e8a422bfdf179542d1aac44e4
Yannick Panis <info@qucode.eu>
2017-06-23 19:01:08 +0200

    initial commit

---
+++ index.jsx
@@ -0,0 +1,186 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+{/*class comps*/}
+
+class Clock extends React.Component{
+  constructor(props) {
+    super(props),
+    this.state = {
+      date: new Date()
+    }
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    return (<h1>{this.state.date.toLocaleTimeString()}</h1>)
+  }
+  tick() {
+      this.setState({date: new Date()})
+  }
+
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props),
+    this.state = {
+      isToggleOn: true
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick () {
+    this.setState ({
+      isToggleOn: !this.state.isToggleOn }
+    )
+  }
+  render () {
+    return (
+      <div>
+        <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "Clock is ON" : "Clock is OFF"}
+        </button>
+        {this.state.isToggleOn &&
+          <Clock />
+        }
+      </div>
+    )
+  }
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props),
+    this.state = {isLoggedIn: false},
+    this.handleLoginClick = this.handleLoginClick.bind(this),
+    this.handleLogoutClick = this.handleLogoutClick.bind(this)
+  }
+  handleLoginClick() {
+    this.setState({isLoggedIn: true})
+  }
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false})
+  }
+  render() {
+    const isLoggedIn = this.state.isLoggedIn
+    let button = null
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {isLoggedIn && <NumberList numbers={[1,2,3,4,5,6]}/>}
+        {button}
+      </div>
+    )
+  }
+}
+
+{/*functional comps*/}
+
+function NumberList(props) {
+  const numbers = props.numbers
+  return (
+    <ul>
+      {numbers.map((number) =>
+        <li key={number.toString()} value={number}>
+          {number}
+        </li>
+      )}
+    </ul>
+  )
+}
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn
+  return (
+    isLoggedIn ? <UserGreeting /> : <GuestGreeting />
+  )
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+function formatDate (date) {
+  return date.toLocaleDateString()
+}
+
+function Avatar (props) {
+  return <img className="Avatar" src={props.avatarUrl} alt={props.name} />
+}
+
+function UserInfo (props) {
+  return (
+    <div className="UserInfo">
+      <Avatar avatarUrl={props.user.avatarUrl} name={props.user.name}/>
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
+
+function CommentText (props) {
+  return <div className="Comment-text">{props.text}</div>
+}
+
+function CommentDate (props) {
+  return <div className="Comment-date">{formatDate(props.date)}</div>
+}
+
+function Comment (props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <CommentText text={props.text} />
+      <CommentDate date={props.date} />
+    </div>
+  )
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I really hope you enjoy learning React - even on your Laptop!',
+  author: {
+    name: 'Hello Kitty Cat',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <Comment date={comment.date} text={comment.text} author={comment.author}/>
+    <Toggle />
+    <LoginControl />
+  </div>,
+  document.getElementById("root")
+)
---
+++ src/index.jsx
@@ -0,0 +1,244 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Nouislider from 'react-nouislider'
+
+class RainbowCanvas extends React.Component {
+  constructor(props) {
+    super(props),
+    this.state = {
+      isDrawing: false,
+      lastX: 0,
+      lastY: 0,
+      hue: 1,
+      direction: true,
+      controlDisplay: "none",
+      controlLeft: "100%",
+      customColor: false,
+      color: "#000000"
+    },
+    this.draw = this.draw.bind(this),
+    this.handleWidth = this.handleWidth.bind(this),
+    this.toggleControls = this.toggleControls.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  canvas () {
+    return document.querySelector("#draw");
+  }
+  ctx () {
+    return this.canvas().getContext("2d");
+  }
+  componentDidMount() {
+    const canvas = this.canvas()
+    const ctx = this.ctx()
+    if(this.props.fullscreen === true){
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
+    ctx.strokeStyle = "#BADA55";
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.lineWidth = 5;
+  }
+  draw(e) {
+    const ctx = this.ctx();
+    let hue = this.state.hue;
+
+    if(this.state.isDrawing){
+      if(this.state.color && this.state.customColor) {
+        ctx.strokeStyle = this.state.color;
+      } else {
+        ctx.strokeStyle = `hsl(${this.state.hue}, 100%, 50%)`;
+      }
+      ctx.beginPath();
+      ctx.moveTo(this.state.lastX, this.state.lastY);
+      ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+      ctx.stroke();
+      hue++
+      if(hue >= 360) {
+        hue = 1
+      }
+      this.setState({
+        hue: hue,
+        lastX: e.nativeEvent.offsetX,
+        lastY: e.nativeEvent.offsetY
+      })
+      this.handleWidth(e);
+    }
+  }
+  handleWidth(e) {
+    const ctx = this.canvas().getContext("2d");
+    let nextState = this.state.direction;
+    if(ctx.lineWidth >= 100 || ctx.lineWidth <= 4) {
+      nextState = !this.state.direction;
+      this.setState({
+        direction: nextState
+      })
+    }
+    if(nextState){
+      ctx.lineWidth++
+    } else {
+      ctx.lineWidth--
+    }
+  }
+  toggleControls () {
+    let onScreen = this.state.controlLeft;
+    let display = this.state.controlDisplay;
+    const fade = () => {
+      onScreen === "0%" ? (
+        this.setState({controlLeft: "100%"})
+      ) : (
+        this.setState({controlLeft: "0%"})
+      )
+    }
+    if((display === "none" && onScreen === "100%") || (display === "block" && onScreen === "0%")) {
+      if(display === "none") {
+        this.setState({controlDisplay: "block"})
+        setTimeout(() => fade(), 0)
+      } else {
+        fade()
+        setTimeout(() => this.setState({controlDisplay: "none"}), 500)
+      };
+    }
+  }
+  handleInputChange(event) {
+    const target = event.target
+    const value = target.type === "checkbox" ? target.checked : target.value
+    const name = target.name
+
+    this.setState({
+      [name]: value
+    })
+  }
+  render () {
+
+    const canvasStyle = {
+      border: "1px solid black"
+    }
+
+    return (
+      <div>
+        <canvas id="draw" width={this.props.width} height={this.props.height} onMouseMove={this.draw}
+        onMouseDown={(e) => {
+          this.setState({
+            isDrawing: true,
+            lastX: e.nativeEvent.offsetX,
+            lastY: e.nativeEvent.offsetY
+          })}
+        } onMouseUp={
+          () => this.setState({isDrawing: false})
+        } onMouseOut={
+          () => this.setState({isDrawing: false})
+        } style={canvasStyle}/>
+        <Controls left={this.state.controlLeft} display={this.state.controlDisplay} canvas={this.canvas} ctx={this.ctx}
+          color={this.state.color} customColor={this.state.customColor} handleInputChange={this.handleInputChange}/>
+        <ButtonOptions onClick={this.toggleControls}/>
+      </div>
+    )
+  }
+}
+
+function ButtonOptions (props) {
+  const buttonStyle = {
+    textAlign: "center",
+    position: "absolute",
+    right: "10px",
+    top: "10px",
+    cursor: "pointer",
+    padding: "8px",
+    color: "white",
+    backgroundColor: "dimgrey",
+    borderRadius: "50%"
+  }
+  return (
+    <div onClick={props.onClick} style={buttonStyle}>
+      <i className="fa fa-cogs" aria-hidden="true"></i>
+    </div>
+  )
+}
+
+function ClearCanvas (props) {
+  const clear = () => {
+    console.log("Clear Canvas")
+    props.ctx().clearRect(0, 0, props.canvas().width, props.canvas().height)
+  }
+  return (
+    <button onClick={clear}>Clear</button>
+  )
+}
+
+function ColorCheckbox (props) {
+  return (
+    <div>
+      <label>
+        <input name="customColor" type="checkbox" onChange={props.handleChange} value={props.checked} />
+        Custom Color
+      </label>
+    </div>
+  )
+}
+
+function ColorPicker (props) {
+  return (
+    <input name="color" type="color" onChange={props.handleChange} value={props.color} />
+  )
+}
+function StrokeCheckbox (props) {
+  return (
+    <div>
+      <label>
+        <input name="customStroke" type="checkbox" onChange={props.handleChange} value={props.checked} />
+        Custom Stroke Width
+      </label>
+    </div>
+  )
+}
+
+function StrokeWidth (props) {
+  return (
+    <Nouislider range={{min: 5, max: 200}} start={[5, 100]} tooltips/>
+  )
+}
+// to-do:
+// checkbox: custom stroke width, dynamic stroke width
+// range: stroke width
+// slider: stroke width if custom
+
+function Controls (props) {
+  const container = {
+    position: "absolute",
+    right: "70px",
+    top: "0",
+    backgroundColor: "transparent",
+    width: "400px",
+    height: "80px",
+    overflow: "hidden",
+    borderRadius: "0 0 5px 5px",
+    display: `${props.display || inlineBlock}`
+  }
+  const content = {
+    backgroundColor: "dimgrey",
+    width: "100%",
+    height: "100%",
+    borderRadius: "0 0 5px 5px",
+    position: "absolute",
+    left: `${props.left || 0}`,
+    transition: "0.5s cubic-bezier(0.22, 0.61, 0.36, 1)"
+  }
+  return (
+    <div style={container}>
+      <div style={content}>
+        <ClearCanvas ctx={props.ctx} canvas={props.canvas}/>
+        <ColorCheckbox checked={props.customColor} handleChange={props.handleInputChange}/>
+        <ColorPicker color={props.color} handleChange={props.handleInputChange}/>
+        <StrokeWidth />
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <div>
+    <RainbowCanvas fullscreen={true}  width="500" height="500"/>
+  </div>,
+  document.getElementById("root")
+)
